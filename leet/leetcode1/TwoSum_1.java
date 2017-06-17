fcada95f80f55487eec1ea688d95648a5becce46
Hangrong Pan <hangrongpan@XiaominsMacBook.home>
2017-06-16 22:30:34 -0400

    add two sum~

---
+++ leet/leetcode1/TwoSum_1.java
@@ -0,0 +1,15 @@
+public class TwoSum_1 {
+  public int [] twoSum(int[] nums, int target) {
+    int[] res = new int[2];
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(target - nums[i])) {
+        res[0] = map.get(target - nums[i]);
+        res[1] = i;
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+    return res;
+  }
+}
