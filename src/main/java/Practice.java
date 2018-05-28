4fa6136d6e7e986531eafabea8e4fae2588e251c
Hangrong Pan <hangrongpan@gmail.com>
2018-05-28 00:21:28 -0400

    update

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -2,10 +2,82 @@ package main.java;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Practice {
+
+
+    static int getMostVisited(int n, int[] sprints) {
+        if (n <= 0 || sprints.length == 0) {
+            return -1;   // return a flag if input is invalid.
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < sprints.length - 1 ; i++) {
+            int start = sprints[i];
+            int end = sprints[i+1];
+            count(res, start, end);
+        }
+        int max = 0;
+        int maxPos = 0;
+        for (int i = 0; i < res.length; i++) {
+            if (res[i] > max) {
+                max = res[i];
+                maxPos = i;
+            }
+        }
+        return maxPos + 1; // based 1 index.
+    }
+
+    static void count(int[] result, int start, int end) {
+        if (start > end) {
+            count(result, end, start);
+        }
+        for (int i = start-1; i <= end-1; i++) {
+            result[i]++;
+        }
+    }
+
+
+    @Test
+    public void testAA() {
+        getMostVisited(10, new int[] {5, 10});
+    }
+
+
+
+    static int countConsecutive2(int N)
+    {
+        // constraint on values of L gives us the
+        // time Complexity as O(N^0.5)
+        int count = 0;
+        for (int L = 1; L * (L + 1) < 2 * N; L++)
+        {
+            float a = (float) ((1.0 * N-(L * (L + 1)) / 2) / (L + 1));
+            if (a-(int)a == 0.0)
+                count++;
+        }
+        return count;
+    }
+    static int consecutive(int num) {
+        int count = 0;
+        for (int i = 1; i * (i+1) < 2 * num; i++) {
+            //Math problem: num = a + (a+1) + (a+2) + ... + (a+i):
+            float a = (float) ((num * 1.0 - (i * (i + 1)) / 2) / (i + 1));
+            if (a - Math.ceil(a) == 0.0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+    @Test
+    public void hackerrank() {
+        System.out.println(consecutive(15));
+    }
 
     @Test
     public void test() {
